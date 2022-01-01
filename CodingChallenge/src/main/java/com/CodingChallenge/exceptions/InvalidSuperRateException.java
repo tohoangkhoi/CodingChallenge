581a7539e815ccd565f010752c15761db80bf695
tohoangkhoi <95118667+tohoangkhoi@users.noreply.github.com>
2022-01-01 18:08:15 +1100

    Update InvalidSuperRateException.java

--- CodingChallenge/src/main/java/com/CodingChallenge/exceptions/InvalidSuperRateException.java
+++ CodingChallenge/src/main/java/com/CodingChallenge/exceptions/InvalidSuperRateException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidSuperRateException extends RuntimeException{
+    //Constructor
     public InvalidSuperRateException(String message) {
         super(message);
     }
